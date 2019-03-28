46f5b852c91d0cba8e0f309a27fa7325872f0783
Howard Li <howard.li@nuvyyo.com>
2019-03-28 13:39:15 -0400

    Add products field to package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "TextTable",
+    products: [
+    	.library(name: "TextTable", targets: ["TextTable"]),
+  	],
     targets: [
-        .target(name: "TextTable", path: "Sources")
+        .target(name: "TextTable", path: "Sources"),
     ]
 )
